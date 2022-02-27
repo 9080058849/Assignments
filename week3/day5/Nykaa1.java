757ddaa65c62f871fc2ffd504ed11670f1511942
Mahesh M <82584871+9080058849@users.noreply.github.com>
2022-02-27 23:09:50 +0530

    Update Nykaa1.java

--- week3/day5/Nykaa1.java
+++ week3/day5/Nykaa1.java
@@ -1,6 +1,8 @@
 package week3.day5;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -12,7 +14,7 @@ import org.openqa.selenium.support.ui.Select;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Nykaa1 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options=new ChromeOptions();
 		ChromeDriver driver=new ChromeDriver(options);
@@ -43,7 +45,9 @@ public class Nykaa1 {
 		driver.findElement(By.xpath("//div[@class='sidebar__inner']//span[text()='Category']")).click();
 		driver.findElement(By.xpath("//span[text()='Category']/following::span[text()='Hair']")).click();
 		driver.findElement(By.xpath("//span[text()='Category']/following::span[text()='Hair Care']")).click();
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("//span[text()='Category']/following::span[text()='Shampoo']")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[text()='Concern']")).click();
 		driver.findElement(By.xpath("//span[text()='Color Protection']")).click();
 		String filter = driver.findElement(By.xpath("//span[@class='filter-value'][1]")).getText();
@@ -55,8 +59,17 @@ public class Nykaa1 {
 		{
 			System.err.println("Shampoo is not selected");
 		}
-		String img = driver.findElement(By.xpath("//div[@id='product-list-wrap']//following::img[1]")).getAttribute("href");
-		driver.get(img);
+		driver.findElement(By.xpath("//div[@id='product-list-wrap']//following::img[1]")).click();
+		//driver.findElement(By.xpath("(//div[@class='css-1rd7vky'])[1]")).click();
+		Thread.sleep(500);
+		
+		Set<String> windowHandles = driver.getWindowHandles();
+		System.out.println(windowHandles);
+		ArrayList<String> value =new ArrayList<String>(windowHandles);
+		driver.switchTo().window(value.get(1));
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);		
+		
 		WebElement size = driver.findElement(By.xpath("//select[@title='SIZE']"));
 		Select dropDown1=new Select(size);
 		dropDown1.selectByVisibleText("175ml");
@@ -67,10 +80,20 @@ public class Nykaa1 {
 		driver.findElement(By.xpath("//button/span[text()='ADD TO BAG']")).click();
 		
 		driver.findElement(By.xpath("//span[text()='Account']//following::button")).click();
-		
 		String grandTotal = driver.findElement(By.xpath("//span[text()='Grand Total']/following::div")).getText();
 		System.out.println("The Grand Total Amount is : "+grandTotal);
 		
-		
+		driver.findElement(By.xpath("//span[text()='PROCEED']")).click();
+		driver.findElement(By.xpath("//button[text()='CONTINUE AS GUEST']")).click();
+		String text = driver.findElement(By.xpath("//div[text()='Grand Total']//following::div")).getText();
+		if(grandTotal.equals(text))
+		{
+			System.out.println("TestCase Pass");
+			driver.close();
+		}
+		else
+		{
+			System.err.println("TestCase Fail");
+		}
 	}
 }
